2002adc8fe2783ea41d178fd42fcce0962c7851f
Alexis Rodríguez Casañas <alu0100711837@ull.edu.es>
2019-07-26 18:17:57 +0100

    preparando el paralelismo

--- README.md
+++
@@ -1,5 +0,0 @@
-# ROSdashCam
-## Ojo, recordar que es necesario cambiar la fuente de vídeo en los ficheros roslaunch, porque llevan mi ruta local.
-
-### Actualizaciones
-- Se han creado más paquetes y se ha añadido un segundo fichero roslaunch. Si se lanza video.launch se ejecuta el detector que se tenía hasta ahora. Si se lanza split_nodes.launch se lanza el detector por un lado (el cual ahora ya solo publica los objetos de tipo box y no la imagen), y el visualizador por otro. El visualidador está suscrito al vídeo, igual que el detector, pero también lo estará a los mensajes "box" de este último, para que pueda pintar las cajitas encima de la imagen. Por último, el visualizador envía la imagen final al nodo image_view
--- src/visualizer/src/display.py
+++ src/visualizer/src/display.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import math
+from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
 
 
 
@@ -20,27 +21,30 @@ class image_converter:
         self.image_pub = rospy.Publisher("visualizer_output",Image)
 
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("image",Image,self.callback)
+        self.image_sub = rospy.Subscriber("image", Image, self.callback)
+        
+        self.boxes_sub = rospy.Subscriber("boxes", Detection2DArray, queue_size = 1)
 
     def callback(self,data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-
+          
+        # Se supone que aqui recibimos el Detection2DArray, que esta compuesto de objetos Object2D para pintarlos
+        # He probado un par de cosas pero no funciona del todo, obtengo tipos de datos raros, obtengo excepciones...
+        # imagino que los tiros deberian ir por algo asi
+        Detection2DArray(self.boxes_sub)
+        # Recorremos el array y para cada objeto obtenemos su propiedad center y size y lo pintamos mediante openCV o lo que sea
+        # ...
         
-
-        (rows,cols,channels) = cv_image.shape
-        if cols > 60 and rows > 60 :
-            cv2.circle(cv_image, (50,50), 10, 255)
-
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except CvBridgeError as e:
             print(e)
 
-        
 
+    
 
 def main(args):
     rospy.init_node('visualizer', anonymous=True)

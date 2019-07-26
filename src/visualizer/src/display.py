f910dd99ef7e2a5cc31d41a50d81211622fb4759
Alexis Rodríguez Casañas <alu0100711837@ull.edu.es>
2019-07-26 22:37:09 +0100

    Implementando mensajes en nodo visualizador

--- src/visualizer/src/display.py
+++ src/visualizer/src/display.py
@@ -6,7 +6,7 @@ roslib.load_manifest('bridge')
 import sys
 import rospy
 import cv2
-from std_msgs.msg import String
+from std_msgs.msg import String, Header
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
@@ -23,20 +23,13 @@ class image_converter:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("image", Image, self.callback)
         
-        self.boxes_sub = rospy.Subscriber("boxes", Detection2DArray, queue_size = 1)
+        self.boxes_sub = rospy.Subscriber("boxes", Detection2DArray, self.drawBoxes, queue_size = 1)
 
     def callback(self,data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-          
-        # Se supone que aqui recibimos el Detection2DArray, que esta compuesto de objetos Object2D para pintarlos
-        # He probado un par de cosas pero no funciona del todo, obtengo tipos de datos raros, obtengo excepciones...
-        # imagino que los tiros deberian ir por algo asi
-        objectsArray =  Detection2DArray(self.boxes_sub)
-        # Recorremos el array y para cada objeto obtenemos su propiedad center y size y lo pintamos mediante openCV o lo que sea
-        # ...
         
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
@@ -44,6 +37,13 @@ class image_converter:
             print(e)
 
 
+    def drawBoxes(self, array):
+        for i in range(len(array.detections)):
+            detectionObject = array.detections[i]
+            bbox = detectionObject.bbox
+            center = bbox.center
+            print("Dibujar caja en: ", center.x, center.y)
+            print("de dimensiones: ", bbox.size_x, bbox.size_y)
     
 
 def main(args):
